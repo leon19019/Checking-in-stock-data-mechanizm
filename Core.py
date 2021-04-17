950ae2ed783a3b3af722741f72a0de00174c7b03
LFES <57556743+leon19019@users.noreply.github.com>
2021-04-17 20:42:06 +0300

    Update

--- Core.py
+++ Core.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from telegram.ext import Updater, CommandHandler
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
 import time
 import telegram_send
 
 
 def get_is_in_stock_data(url = "https://www.computeruniverse.net/ru/asrock-barebone-4x4-box-v1000m-cpu-v1605b-5xusb2xdphdmi", class_name = "div.product-stock__delivery-text"):
 
-    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+
+    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_options=firefox_options)
 
     browser.get(url)
     time.sleep(2)
